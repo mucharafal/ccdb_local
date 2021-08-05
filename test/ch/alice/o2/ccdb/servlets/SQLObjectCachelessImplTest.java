cd4eb04e0cbf5185113e3e367e877b968d008399
rmucha <mucharafal44@gmail.com>
2021-08-05 13:24:41 +0200

    Add new test for update

--- test/ch/alice/o2/ccdb/servlets/SQLObjectCachelessImplTest.java
+++ test/ch/alice/o2/ccdb/servlets/SQLObjectCachelessImplTest.java
@@ -27,4 +27,27 @@ class SQLObjectCachelessImplTest extends SQLObjectTest {
 
         saveInDatabase(toInsert, 5, 2);
     }
+
+    @Test
+    void updateObjectAfterUpdatePath() {
+        SQLObject objectWithPathA = SQLObject.getObject(object3Id);
+        SQLObject objectWithPathX = SQLObject.getObject(object1Id);
+
+        assertNotNull(objectWithPathA);
+        assertNotNull(objectWithPathX);
+
+        try(DBFunctions db = SQLObject.getDB()) {
+            db.query("update ccdb_path set pathid = 30 where path = 'x';");
+            db.query("update ccdb_path set pathid = 100 where path = 'a';");
+            db.query("update ccdb_path set pathid = 101 where path = 'x';");
+        }
+
+        objectWithPathA.setPath("x");
+        objectWithPathX.setPath("a");
+        saveInDatabase(objectWithPathA, 4, 2);
+        saveInDatabase(objectWithPathX, 4, 2);
+
+        assertEquals("x", SQLObject.getObject(objectWithPathA.id).getPath());
+        assertEquals("a", SQLObject.getObject(objectWithPathX.id).getPath());
+    }
 }
